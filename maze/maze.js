f1570426a9cb0724a6990ebcdecb73dc454a65db
wdrzazga <popownica@gmail.com>
2024-12-10 18:47:56 +0100

    other maze files uploaded

---
+++ maze/maze.js
@@ -0,0 +1,79 @@
+const maze = [
+    [1, 'S', 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 1, 0, 0, 0, 0, 1, 1],
+    [1, 0, 1, 1, 0, 1, 1, 0, 0, 1],
+    [1, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+    [1, 1, 1, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 1, 1, 1, 1, 1, 0, 1, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 'E', 1],
+];
+
+const mazeContainer = document.getElementById('maze');
+
+
+function drawMaze(player) {
+	mazeContainer.innerHTML = '';
+    for (let row = 0; row < maze.length; row++) {
+        for (let col = 0; col < maze[row].length; col++) {
+            const cell = document.createElement('div');
+
+            if (maze[row][col] === 1) {
+                cell.classList.add('wall');
+                cell.textContent = '';
+            } else if (maze[row][col] === 0) {
+                cell.classList.add('path');
+				cell.innerHTML =  player.x === col && player.y === row ?'<img src="player.png"/>' : '';                
+            } else if (maze[row][col] === 'S') {
+                cell.classList.add('start');
+                cell.textContent = 'S'; // Start point
+				cell.innerHTML =  player.x === col && player.y === row ?'<img src="player.png"/>' : '';
+            } else if (maze[row][col] === 'E') {
+                cell.classList.add('end');
+                cell.textContent = 'E'; // End point
+				cell.innerHTML =  player.x === col && player.y === row ?'<img src="happyPlayer.png"/>' : '';
+            }
+
+            mazeContainer.appendChild(cell);
+        }
+    }
+}
+
+class Player {
+	constructor() {
+		this.x = 3;
+		this.y = 5;
+	}
+}
+
+const player = new Player();
+drawMaze(player);
+
+function move(event){
+	console.log(event);
+	if (event.key === 'w') {
+		if (player.y > 0)
+			if (maze[player.y-1][player.x] == 0)
+				player.y -= 1;
+	}
+	else if (event.key === 's') {
+		player.y ++;
+	}
+	else if (event.key === 'a') {
+		if (player.x > 0) {
+			if (maze[player.y][player.x-1] == 0) player.x --;
+		}
+	}
+	else if (event.key === 'd') {
+		player.x ++;
+	}
+	
+	writeCoords(player);
+	drawMaze(player);	
+}
+
+function writeCoords(player){
+	document.getElementById("coordinates").innerText = `x: ${player.x} y: ${player.y}`;
+}
